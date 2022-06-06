36708691cb0dc035bfa5963ac28347083b3fbddf
+ <+>
2022-06-06 06:31:54 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+
+function writeCards(names, eventName){
+const cards = [];
+for(let i = 0; i<names.length;i++)
+}
+
+cards.push("Thank you, "+names[i] + ",for the wonderful " + eventName + "gift!");
+
+
+}
+return cards;
+}
+
+function countDown(int){
+while(int >= 0){
+    console.log(int--);
+}
+}
